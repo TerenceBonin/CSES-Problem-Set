1d444ba77c59286100700e0cf117cfbcb720c9b7
TerenceBonin <terence.bon@hotmail.fr>
2022-06-24 23:33:35 +0200

    yup

--- Introduction/src/weird_algorithm.rs
+++ Introduction/src/weird_algorithm.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+
+    io::stdin().read_line(&mut buffer).expect("Failed to read line");
+    let mut x: i64 = buffer.trim().parse().expect("Input not an integer");
+    println!("{}",x);
+    while x != 1 {
+        match x%2{
+            0=>{x /= 2;
+                println!("{}",x)
+            }
+            1=>{x = 3*x+1;
+                println!("{}",x)
+            }
+            _=>println!("alrightlol")
+        }
+    }
 }
