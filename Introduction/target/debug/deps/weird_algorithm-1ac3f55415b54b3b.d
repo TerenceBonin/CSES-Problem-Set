b68130440899081f672aac274024cd5e9c965991
TerenceBonin <terence.bon@hotmail.fr>
2022-06-24 22:21:45 +0200

    yup

---
+++ Introduction/target/debug/deps/weird_algorithm-1ac3f55415b54b3b.d
@@ -0,0 +1,5 @@
+C:\Users\MOI MEME\rustfolder\CSES-Problem-Set\Introduction\target\debug\deps\weird_algorithm-1ac3f55415b54b3b.rmeta: src/weird_algorithm.rs
+
+C:\Users\MOI MEME\rustfolder\CSES-Problem-Set\Introduction\target\debug\deps\weird_algorithm-1ac3f55415b54b3b.d: src/weird_algorithm.rs
+
+src/weird_algorithm.rs:
